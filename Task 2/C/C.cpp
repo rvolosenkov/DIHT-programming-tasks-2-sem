44105acc6a1c5dc15dcebe58dc4df478b3d28f03
rvolosenkov <47807398+rvolosenkov@users.noreply.github.com>
2019-04-09 18:19:03 +0300

    Update C.cpp

--- Task 2/C/C.cpp
+++ Task 2/C/C.cpp
@@ -9,65 +9,72 @@
 
 class FindSequenceOfMoves { // solver
 public:
-    FindSequenceOfMoves(std::vector<int> StartState) : startState(StartState), correctState({1, 2, 3, 4, 5, 6, 7, 8, 0}) {}
+    FindSequenceOfMoves(int StartState) : startState(StartState), correctState(123456780) {}
     bool SearchForCorrectState(std::string& answer); // main solving function
 
 private:
-    std::vector<int> startState; // vector for start state, filled upside down, from left to right
-    std::vector<int> correctState; // vector for correct state
-    std::map<std::vector<int>, std::string> ways; // storage of state+way how this state was reached from the start (i.e. UDDL means you should move up, down, down and left from the start to reach the current state)
-    int findPos(std::vector<int>& currState); // returns a number of zero position (i.e. for correct state returns 8)
+    int startState; // number for start state, filled upside down, from left to right
+    int correctState; // number for correct state
+    std::map<int, std::string> ways; // storage of state+way how this state was reached from the start (i.e. UDDL means you should move up, down, down and left from the start to reach the current state)
+    int findPos(int currState); // returns a number of zero position (i.e. for correct state returns 8)
     int countDisorders(); // returns a number of disorders in the start state; this is necessary to figure out whether the start state is solvable
-    void getNeighbors(std::vector<int>& currState, std::vector<std::vector<int>>& neighbors); // search all neighboring states (in relation to the current one)
+    void getNeighbors(int currState, std::vector<int>& neighbors); // search all neighboring states (in relation to the current one)
 };
 
-int FindSequenceOfMoves::findPos(std::vector<int>& currState) {
-    for (int i = 0; i < 9; ++i) {
-        if (currState[i] == 0) {
+int FindSequenceOfMoves::findPos(int currState) {
+    for (int i = 8; i >= 0; --i) {
+        if (currState % 10 == 0) {
             return i;
         }
+        currState /= 10;
     }
 }
 
 int FindSequenceOfMoves::countDisorders() {
-    int result = 0;
-    for (int i = 0; i < 8; ++i) {
-        for (int j = i + 1; j < 9; ++j) {
-            if (startState[i] > startState[j]) {
+    int result = 0, state = startState;
+    std::vector<int> start;
+    for (int i = 0; i < 9; ++i) {
+        start.push_back(state % 10);
+        state /= 10;
+    }
+    for (int i = 8; i >= 1; --i) {
+        for (int j = i - 1; j >= 0; --j) {
+            if (start[i] < start[j]) {
                 ++result;
             }
         }
     }
+    start.clear();
     return result;
 }
 
-void FindSequenceOfMoves::getNeighbors(std::vector<int>& currState, std::vector<std::vector<int>>& neighbors) {
+void FindSequenceOfMoves::getNeighbors(int currState, std::vector<int>& neighbors) {
     int pos = this -> findPos(currState);
+    int tenInPowerPos = 1;
+    for (int i = 0; i < 8 - pos; ++i) {
+        tenInPowerPos *= 10;
+    }
     if (pos > 2) { // if zero is not in the top line, we can move it up
-        std::vector<int> createdState = currState;
-        std::swap(createdState[pos], createdState[pos - 3]);
-        neighbors.push_back(createdState);
+        int sufferedDigit = (currState / (tenInPowerPos * 1000)) % 10;
+        neighbors.push_back(currState - sufferedDigit * tenInPowerPos * 999);
     }
     if (pos < 6) { // if zero is not in the bottom line, we can move it down
-        std::vector<int> createdState = currState;
-        std::swap(createdState[pos], createdState[pos + 3]);
-        neighbors.push_back(createdState);
+        int sufferedDigit = (currState / (tenInPowerPos / 1000)) % 10;
+        neighbors.push_back(currState - sufferedDigit * (tenInPowerPos / 1000) + sufferedDigit * tenInPowerPos);
     }
     if (pos % 3 != 0) { // if zero is not in the left line, we can move it left
-        std::vector<int> createdState = currState;
-        std::swap(createdState[pos], createdState[pos - 1]);
-        neighbors.push_back(createdState);
+        int sufferedDigit = (currState / (tenInPowerPos * 10)) % 10;
+        neighbors.push_back(currState - sufferedDigit * tenInPowerPos * 9);
     }
     if (pos % 3 != 2) { // if zero is not in the right line, we can move it right
-        std::vector<int> createdState = currState;
-        std::swap(createdState[pos], createdState[pos + 1]);
-        neighbors.push_back(createdState);
+        int sufferedDigit = (currState / (tenInPowerPos / 10)) % 10;
+        neighbors.push_back(currState - sufferedDigit * (tenInPowerPos / 10) + sufferedDigit * tenInPowerPos);
     }
 }
 
 bool FindSequenceOfMoves::SearchForCorrectState(std::string& answer) { // main solving function
-    std::queue<std::vector<int>> newStates; // queue for all noticed but unviewed states
-    std::set<std::vector<int>> allStates; // set for all noticed states
+    std::queue<int> newStates; // queue for all noticed but unviewed states
+    std::set<int> allStates; // set for all noticed states
     if ((countDisorders() + findPos(startState)) % 2 != 0) { // check an ability to solve
         return false;
     }
@@ -78,9 +85,9 @@ bool FindSequenceOfMoves::SearchForCorrectState(std::string& answer) { // main s
     allStates.insert(startState);
     ways.insert(std::make_pair(startState, "")); // start state don't require some moves to reach the same one => a string is empty
     while (newStates.empty() == false) {
-        std::vector<int> currState = newStates.front(); // view next state
+        int currState = newStates.front(); // view next state
         newStates.pop();
-        std::vector<std::vector<int>> neighbors;
+        std::vector<int> neighbors;
         getNeighbors(currState, neighbors); // find all neighbors of the current state
         for (int i = 0; i < neighbors.size(); ++i) { // look through all neighboring states
             if (allStates.count(neighbors[i]) == 0) { // work with this neighboring state in case it has not been viewed yet
@@ -114,11 +121,11 @@ bool FindSequenceOfMoves::SearchForCorrectState(std::string& answer) { // main s
 }
 
 int main() {
-    std::vector<int> StartState;
+    int StartState = 0;
     for (int i = 0; i < 9; ++i) {
         int value;
         std::cin >> value;
-        StartState.push_back(value);
+        StartState = StartState * 10 + value;
     }
     FindSequenceOfMoves SolveBarleyBreak(StartState);
     std::string answer;
